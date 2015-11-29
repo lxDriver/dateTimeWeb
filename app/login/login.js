287d67b70462cc8913e23994d35f58152a7bd4fc
Julian Kleine <info@lxdriver.de>
2015-11-29 20:54:52 +0100

    Merge branch 'master' into dev
    # Conflicts:
    #	app/login/login.js

--- app/login/login.js
+++ app/login/login.js
@@ -19,23 +19,21 @@ angular.module('dateTime.login', ['ngRoute', 'firebase'])
  
 // Home controller
 .controller('LoginCtrl', ['$scope','$location', '$firebaseAuth', function($scope,$location,$firebaseAuth) {
-    
-    var firebaseObj = new Firebase("https://lxdatetime.firebaseio.com"); 
-    
+    var firebasePath = "https://lxdatetime.firebaseio.com/";
+    var firebaseObj = new Firebase(firebasePath); 
+
     var loginObj = $firebaseAuth(firebaseObj);
-    
+
     $scope.user = {};
-    
+
     $scope.SignIn = function(event) {
         event.preventDefault();  // To prevent form refresh
-        
+
         console.log('Authentication started');
-        
+
         var username = $scope.user.email;
         var password = $scope.user.password;
-        
-        
-        
+
         loginObj.$authWithPassword({
                 email: username,
                 password: password
@@ -48,5 +46,53 @@ angular.module('dateTime.login', ['ngRoute', 'firebase'])
                 // Failure callback
                 console.log('Authentication failure');
             });
-    }    
+    }
+
+
+    // find a suitable name based on the meta info given by each provider
+    var getName = function(authData) {
+      switch(authData.provider) {
+         case 'password':
+           return authData.password.email.replace(/@.*/, '');
+         case 'twitter':
+           return authData.twitter.displayName;
+         case 'facebook':
+           return authData.facebook.displayName;
+      }
+    }
+
+    var firebaseUsers = firebasePath + "users/";
+
+        // Tests to see if /users/<userId> has any data. 
+    var checkIfUserExists = function(userId) {
+        var usersRef = new Firebase(firebaseUsers);
+        usersRef.child(userId).once('value', function(snapshot) {
+            var exists = (snapshot.val() !== null);
+            
+            if(exists) {
+                console.log("User does exist.");
+            } else {
+                console.log("User does not exist.");
+            }
+            return exists;
+        });
+    }
+
+    firebaseObj.onAuth(function(authData) {
+        var isUser = checkIfUserExists(authData.uid);
+        console.log("onAuth");
+        if (authData && !isUser) {
+            // save the user's profile into the database so we can list users,
+            // use them in Security and Firebase Rules, and show profiles
+            firebaseObj.child("users").child(authData.uid).set({
+              provider: authData.provider,
+              name: getName(authData)
+            });
+        } 
+        if (authData) {
+            console.log("existing user forwarded");
+            $location.path('/dashboard');
+        }
+    });
+    
 }]);
